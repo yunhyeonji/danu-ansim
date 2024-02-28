c4c0ac3bf153fbab56f8b6ee0b93582189271b75
yhj <yhj@danusys.com>
2024-02-28 17:39:13 +0900

    [2024.02.28]윤현지 - 카메라 권한 요청 추가

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,42 +1,49 @@
-import React, {useEffect, useState, useRef} from 'react';
-import {StyleSheet, View, Button} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {View, Alert} from 'react-native';
 import {
   Camera,
   useCameraPermission,
   useCameraDevice,
-  FlashMode,
+  NoCameraDeviceError,
 } from 'react-native-vision-camera';
 
 export default function CameraScreen() {
   const cameraRef = useRef(null);
   const {hasPermission, requestPermission} = useCameraPermission();
   const device = useCameraDevice('back');
-  if (device == null) return <NoCameraDeviceError />;
 
-  // 플래쉬 켜기
-  const toggleFlash = async () => {
-    // try {
-    //   const photo = await camera.current.takePhoto({
-    //     flash: on,
-    //   });
-    // } catch (error) {
-    //   console.error('Failed to toggle flash:', error);
-    // }
-  };
+  useEffect(() => {
+    const requestCameraPermission = async () => {
+      const granted = await requestPermission();
+      if (!granted) {
+        Alert.alert(
+          'Permission Denied',
+          'Camera permission is required to use the camera.',
+        );
+      }
+    };
+
+    requestCameraPermission();
+  }, []);
+
+  if (!device) {
+    return <NoCameraDeviceError />;
+  }
 
   return (
     <View style={{flex: 1}}>
-      <Camera
-        style={{flex: 1}}
-        device={device}
-        isActive={true}
-        ref={cameraRef}
-        autoFocus="on"
-        flash="off"
-      />
-      <View style={{position: 'absolute', bottom: 20, alignSelf: 'center'}}>
-        <Button title="Toggle Flash" onPress={toggleFlash} />
-      </View>
+      {hasPermission ? (
+        <Camera
+          style={{flex: 1}}
+          device={device}
+          isActive={true}
+          ref={cameraRef}
+          autoFocus="on"
+          flash="auto"
+        />
+      ) : (
+        <></>
+      )}
     </View>
   );
 }
